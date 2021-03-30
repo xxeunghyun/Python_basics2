929c6778ae71daff312d4db20a1a35de44f8f16e
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-03-30 23:56:13 +0800

    Add files via upload

---
+++ 01. wea.py
@@ -0,0 +1,14 @@
+s = [1, 'four', 9, 16, 25]
+
+print(s)
+print(s[1]) #four
+print(len(s)) #5
+
+s[1] = 4
+print(s)
+
+del s[2] # delete 9
+print(s)
+
+s.append('egoing') #appended at the last
+print(s)
